4e6ccfc3e92ee81c6d943bb603cce04cb2549638
Burhan-Khawaja <45739689+Burhan-Khawaja@users.noreply.github.com>
2020-12-09 20:33:26 -0500

    part 3 - except marker making
    implemented map on the contact page but no markers appear on the map yet

---
+++ public/javascripts/geocoding.js
@@ -0,0 +1,84 @@
+//private mapbox token
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYmtoYXdhamEiLCJhIjoiY2tpMTJkM3VvMHZxcjJ5cGVsanA2aXg2OSJ9.lXT1twUQoyk9dJyp5ElbQw'
+//first part of url string to make requests for geocoding
+const MAPBOX_REQUEST_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
+let myMap;
+
+const addLocation = async () => {
+    console.log("IN GEOCODING JAVASCRIPT FILE.")
+    //access html form to later append address
+    let form = document.getElementById("myForm");
+    //access address elements.
+    let street = document.getElementById("street");
+    let city   = document.getElementById("city");
+    let state  = document.getElementById("state");
+    let zip    = document.getElementById("zip")
+    
+    let address = street.value + " " + city.value + ", " + state.value + " " + zip.value;
+    console.log("Address : " + address);
+
+    const normalized = encodeURIComponent(address);
+
+    const tmpUrl = MAPBOX_REQUEST_URL + normalized + '.json?access_token=' +  MAPBOX_TOKEN;
+    console.log(tmpUrl);
+
+    let responseLocationArray;
+    //issue AJAX axios request to mapbox server to retireve location of address in longitide/latitude. 
+    //store long/lat array into responseLocationArray.
+    const mapbox_response = await axios.get(tmpUrl)
+        .then(function (response) {
+            //on response, store the array of longitude/latitude in variable for easier access.
+            responseLocationArray = response.data.features[0].center;           
+            L.marker( [responseLocationArray[0], responseLocationArray[1]] ).addTo(myMap);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+    //create variable for longitude, store value from responseLocationArray
+    //to longitude in string form as a text node, and set longitude in form to value???
+    let tmpLongitude = document.createTextNode( responseLocationArray[0].toString() );
+    var formLongitude = document.getElementById("long");
+    formLongitude.value = responseLocationArray[0];
+        
+    //same algorithm as above for latitude
+    let tmpLatitude = document.createTextNode(responseLocationArray[1].toString());
+    var formLatitude = document.getElementById("lat");
+    formLatitude.value = responseLocationArray[1];
+        
+    form.submit();
+}
+
+
+/*
+//
+//  NAME
+//  createMap () -  Create the map to display on html page using leaflet. createMap will be called in app.js file beacuse we need to create the map
+//                  before the user enters in addresses, and not when the user requests the contacts page.
+//                  This is so when the user requests the contacts page, if a new map is created then we will lose access to the markers from before.
+//
+*/
+ const createMap = async() => {
+     console.log("Creating map.");
+        myMap = L.map('myMap').setView([41, -74], 10);
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiYmtoYXdhamEiLCJhIjoiY2tpMTJkM3VvMHZxcjJ5cGVsanA2aXg2OSJ9.lXT1twUQoyk9dJyp5ElbQw'
+        }).addTo(myMap);
+        console.log("finsihed creating map!");
+    }
+    
+    
+
+
+//problem: createMap is called everytime contacts page requested, making all markers dissapear
+//solutions:
+// every time contacts gets called go through database adn add all markers to map
+//create map on server start.
+//
+
