51743e1880d38ce7c374aa55db76c4a94210eb5e
Burhan-Khawaja <45739689+Burhan-Khawaja@users.noreply.github.com>
2020-12-15 02:34:49 -0500

    Part 4 - CRUD
    Created event handler for update contact link.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const { Db } = require('mongodb');
 const { getContactsCollection } = require('../public/javascripts/database-setup');
 var router = express.Router();
 let mongoDatabase = require('../public/javascripts/database-setup');
-
 /* GET start page. 
    I'm allowing the user to hit multiple URLs and 
    still get the same functionality.  I could also
@@ -18,19 +17,26 @@ router.get('/mailer', function(req, res, next) {
 
 //retrieve mailer page that contains form
 router.post('/mailer.html', function(req,res,next) {  
-    console.log("Posting daata");  
+    console.log("Posting Data");  
     var body = req.body;
     //BURBUR should process data to be better.
     console.log(body);
     mongoDatabase.getContactsCollection().insertOne(body);
-    console.log("submitted data to database");
+    console.log("Completed Submittion of Data to Database");
+    res.render("submission", {});
 });
 
 //get contacts list
 router.get('/contacts', function(req, res, next) {
     mongoDatabase.getContactsCollection().find().toArray(function (err, result) {
+        console.log(result);
         res.render("contacts", {responseList: result} );
     });    
+});
+
+router.get('/update', function(req, res, next) {
+    console.log("gETINNG UPDATE PAGE WAITAMINUTE");
+    res.render("update", {});
 });
     module.exports = router;
 
