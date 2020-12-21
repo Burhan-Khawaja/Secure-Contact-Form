d9c63e90336d05444a61365092283da2268399eb
Burhan-Khawaja <45739689+Burhan-Khawaja@users.noreply.github.com>
2020-12-21 14:16:44 -0500

    Part 7
    Bootstrap and CSS implementation completed.

--- public/javascripts/geocoding.js
+++ public/javascripts/geocoding.js
@@ -1,3 +1,6 @@
+//geocoding.JS will handle the geocoding aspect of the project as well as the map creation, marker addition, and deleting and updating contacts
+//this is client side javascript
+
 //private mapbox token
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiYmtoYXdhamEiLCJhIjoiY2tpMTJkM3VvMHZxcjJ5cGVsanA2aXg2OSJ9.lXT1twUQoyk9dJyp5ElbQw'
 //first part of url string to make requests for geocoding
@@ -6,8 +9,13 @@ const MAPBOX_REQUEST_URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
 //let mongoDatabase = require('./database-setup');
 let myMap;
 
+
+/*
+//  NAME
+//  addLocation () - Adds the longitude/latitude location of the users address to the form before submitting it.        
+*/
 const addLocation = async () => {
-    console.log("Adding location.");
+
     //access html form to later append address
     let form = document.getElementById("myForm");
     //access address elements.
@@ -16,7 +24,8 @@ const addLocation = async () => {
     let state  = document.getElementById("state");
     let zip    = document.getElementById("zip")
     
-    let address = street.value + " " + city.value + ", " + state.value + " " + zip.value;
+    var address = street.value + " " + city.value + ", " + state.value + " " + zip.value;
+    //create url with no spaces, then append base request url, normalized address, and token to form a url.
     const normalized = encodeURIComponent(address);
     const tmpUrl = MAPBOX_REQUEST_URL + normalized + '.json?access_token=' +  MAPBOX_TOKEN;
 
@@ -32,13 +41,12 @@ const addLocation = async () => {
             console.log(error);
         });
 
-    //create variable for longitude, store value from responseLocationArray
-    //to longitude in string form as a text node, and set longitude in form to value???
-    var formLongitude = document.getElementById("long");
+    //get 2 form elements long and lat in document and set them according to what mapbox sent back    
+    let formLongitude = document.getElementById("long");
     formLongitude.value = responseLocationArray[0];
         
     //same algorithm as above for latitude
-    var formLatitude = document.getElementById("lat");
+    let formLatitude = document.getElementById("lat");
     formLatitude.value = responseLocationArray[1];
 
     form.submit();
@@ -48,12 +56,10 @@ const addLocation = async () => {
 /*
 //
 //  NAME
-//  createMap () -  Create the map to display on html page using leaflet. createMap will be called in app.js file beacuse we need to create the map
-//                  before the user enters in addresses, and not when the user requests the contacts page.
-//                  This is so when the user requests the contacts page, if a new map is created then we will lose access to the markers from before.
+//  createMap () -  Create the map to display on html page using leaflet. 
 //
 */
-const createMap = async(long,lat) => {
+const createMap = async() => {
     myMap = await L.map('myMap').setView([41, -74], 10);
 
     await L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -69,43 +75,62 @@ const createMap = async(long,lat) => {
 /*
 //
 //  NAME
-//  addMarker (long,lat) -  Add markers to map. This function is called in contacts.pug file. 
-//                          funciton is supplied longitiude and latitude elements to create marker.
-//                  
+//  addMarker (long,lat) -  Add markers to map. Also creates event handler to display contact tooltip on hover.
+//                           called in contacts.pug file. 
+//                                            
 //  PARAMS-                 
 //          long - longitude, retrieved from mongoDB database
 //          lat - latitude, also supplied from databaswe
 */  
-const addMarker = async(long,lat) => {
-    await L.marker( [lat,long] ).addTo(myMap);
+const addMarker = async(response) => {
+    let marker = await L.marker( [response.latitude,response.longitude] );
+    //create some html that will be shown when hovered over the marker.
 
-    //access html form to later append address
-    let form = document.getElementById("myForm");
-    //access address elements.
-    let street = document.getElementById("street");
-    let city   = document.getElementById("city");
-    let state  = document.getElementById("state");
-    let zip    = document.getElementById("zip")
+    //create paragraph elements for name, address, phone, email, and text elements for the text.
+    //append the text to the paragraph elements, then append the paragraph elements to a div.
+    let name = document.createElement('p');
+    let nameText = document.createTextNode(response.first + " " + response.last);
+    name.append(nameText);
 
+    let address = document.createElement('p');
+    let addressText = document.createTextNode(response.street +" " + response.city + ", " + response.state + " " + response.zip);
+    address.append(addressText);
+
+    let phone = document.createElement('p');
+    let phoneText = document.createTextNode(response.phone)
+    phone.append(phoneText);
+    
+    let email = document.createElement('p');
+    let emailText = document.createTextNode(response.email);
+    email.append(emailText);
+
+    //create div and append paragraphs to it
+    let popupContent = document.createElement('div');
+    popupContent.append(name);
+    popupContent.append(address)
+    popupContent.append(phone)
+    popupContent.append(email)
+    popupContent.className = 'mapMarkerTooltip'
+
+    marker.bindPopup(popupContent).openPopup();
+
+    marker.on('mouseover',function(ev) {
+        marker.openPopup();
+    });
+    marker.addTo(myMap)
 }
 
-//function that will post to server to recieve contact form with pre populated data.
-//Will submit an invisible form to server.
-function changeContact(contactID) {
-    console.log("In change contact function. Welcome");
-    console.log(contactID);
-    //BURBUR legacy code. 
-    //IF YOU DELETE THIS CODE DONT FORGET TO REMOVE 
-    //AXIOS REQUIRE IN PUG FILE
-    // axios.post("/TEST", contact)
-    // .then(function (response) {
-    //     console.log("Back in geocoding file, recieve axiosres");
-    //     console.log(response);
-    //     window.location = "/update";
-    // });
-    //console.log(contact._id)
-    
 
+/*
+//
+//  NAME
+//      changeContact (contactID) - called when user wants to update contact.
+//                                  Create invisible form to post to server with the _id of the contact to be updated
+//                                            
+//  PARAMS-                 
+//          contactID - mongoDB _id object so the server can look up the contact and pre populate the form.
+*/  
+function changeContact(contactID) {
     //Create a form with a hidden element that stores the contact
     //ID value which will be named _id
     //submit the form via  post request to /TEST
@@ -121,11 +146,20 @@ function changeContact(contactID) {
     form.submit();
 }
 
-//function that creates a form to be submitted that will be responsbile for deleting a contact form the database.
-//This function is called in contacts.pug as an event handler, and will post to /deleteContact in index.js which will delete the contact. 
+
+/*
+//
+//  NAME
+//      changeContact (contactID) - creates a form to be submitted that will be responsbile for deleting a contact from the database.
+//                                  called in contacts.pug as an event handler, and will post to /deleteContact in index.js. 
+//                                            
+//  PARAMS-                 
+//          contactID - mongoDB _id object so the server can look up the contact and delete it..
+*/  
 function deleteContact(contactID) {
-    console.log("In DELETE contact function. Welcome");
-    console.log(contactID);
+    //Create a form with a hidden element that stores the contact
+    //ID value which will be named _id
+    //submit the form via  post request to /TEST
     let form = document.createElement('form');
     form.method = 'post';
     form.action = '/deleteContact';
@@ -138,21 +172,24 @@ function deleteContact(contactID) {
     form.submit();
 }
 
-
+/*
+//
+//  NAME
+//      searchTable () - search "name" column of the table when the user presses a key in the search bar. 
+//                                            
+*/ 
 function searchTable() {   
-    var input, filter, tbl, tr, td, i, txtValue;
-    input = document.getElementById("searchBox");
-    filter = input.value.toUpperCase();
-    tbl = document.getElementById("contactList");
-    tr = tbl.getElementsByTagName("tr");
+    let input = document.getElementById("searchBox");
+    let filter = input.value.toUpperCase();
+    let tbl = document.getElementById("contactList");
+    let tr = tbl.getElementsByTagName("tr");
 
     // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-    //we have hidden longitude/latitude elements in each table row, so we get the 3rd td element which is the name
-    td = tr[i].getElementsByTagName("td")[2];
-    console.log(td);
+    for (let i = 0; i < tr.length; i++) {
+    //we have hidden longitude/latitude elements in each table row, so we get the 3rd td element to access the name
+    let td = tr[i].getElementsByTagName("td")[2];
     if (td) {
-        txtValue = td.textContent;
+        let txtValue = td.textContent;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             tr[i].style.display = "";
         }
